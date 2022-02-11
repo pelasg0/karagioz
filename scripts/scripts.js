1fa24023a2add6734a14d686b0d8a86f2441f22b
MrDrawin <mrdrawin@abv.bg>
2022-02-11 15:30:23 +0100

    gallery +

---
+++ scripts/scripts.js
@@ -0,0 +1,10 @@
+var counter = 1; 
+setInterval(function()
+{
+    document.getElementById('landscape-radio' + counter).checked = true;
+    counter ++;
+    if(counter > 3)
+    {
+        counter = 1;
+    }
+}, 5000);
