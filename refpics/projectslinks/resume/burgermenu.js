c317efd9050ac8f0095f9938655953c961887844
MrDrawin <mrdrawin@abv.bg>
2022-10-22 20:12:27 +0200

    dada

--- refpics/projectslinks/resume/burgermenu.js
+++
@@ -1,9 +0,0 @@
-//----------------------------------FLOWER BURGER MENU-------------------------------------------//
-const hamburger = document.querySelector(".nav-bar-button-wrapper");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
